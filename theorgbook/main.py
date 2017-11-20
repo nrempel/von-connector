a0e7c66771648ffc8d4d4bf2bcc86b0f7e606252
Nicholas Rempel <nbrempel@gmail.com>
2017-11-20 09:51:40 -0800

    fix pool not persisting between requests

--- theorgbook/main.py
+++ theorgbook/main.py
@@ -6,75 +6,61 @@ import time
 import json
 
 from sanic import Sanic
-from sanic.response import json
+from sanic.response import json as sanic_json
 from von_agent.nodepool import NodePool
-from von_agent.demo_agents import TrustAnchorAgent, SRIAgent, OrgBookAgent, BCRegistrarAgent
+from von_agent.demo_agents import OrgBookAgent
 
 app = Sanic(__name__)
 
 
+async def boot():
+    global pool
+    global orgbook
+
+    pool = NodePool(
+        # Hack to use different pool names. Agent lib doesn't support
+        # reopening existing pool config
+        'theorgbook' + str(calendar.timegm(time.gmtime())),
+        '/home/indy/.indy-cli/networks/sandbox/pool_transactions_genesis')
+    await pool.open()
+    orgbook = OrgBookAgent(
+        pool,
+        'The-Org-Book-Agent-0000000000000',
+        'the-org-book-agent-wallet',
+        None,
+        '127.0.0.1',
+        9702,
+        'api/v0')
+    await orgbook.open()
+    await orgbook.create_master_secret('secret')
+
+
 @app.route("/")
 async def test(request):
-    return json({"hello": "world"})
+    return sanic_json({"hello": "world"})
 
 
 @app.route("/get-claim-request", methods=['POST'])
 async def get_claim_request(request):
-
-    pool = NodePool(
-        # Hack to use different pool names. Agent lib doesn't support
-        # reopening existing pool config
-        'theorgbook' + str(calendar.timegm(time.gmtime())),
-        '/home/indy/.indy-cli/networks/sandbox/pool_transactions_genesis')
-    await pool.open()
-
-    obag = OrgBookAgent(
-        pool,
-        'The-Org-Book-Agent-0000000000000',
-        'the-org-book-agent-wallet',
-        None,
-        '127.0.0.1',
-        9702,
-        'api/v0')
-    await obag.open()
-    await obag.create_master_secret('secret')
-
     did = request.json['did']
     seqNo = request.json['seqNo']
     claim_def_json = request.json['claim_def']
 
-    await obag.store_claim_offer(did, seqNo)
-    claim_req_json = await obag.store_claim_req(did, claim_def_json)
-    await pool.close()
+    await orgbook.store_claim_offer(did, seqNo)
+    claim_req_json = await orgbook.store_claim_req(did, claim_def_json)
 
-    return json(claim_req_json)
+    return sanic_json(claim_req_json)
 
 
 @app.route("/store-claim", methods=['POST'])
 async def store_claim(request):
-    pool = NodePool(
-        # Hack to use different pool names. Agent lib doesn't support
-        # reopening existing pool config
-        'theorgbook' + str(calendar.timegm(time.gmtime())),
-        '/home/indy/.indy-cli/networks/sandbox/pool_transactions_genesis')
-    await pool.open()
+    await orgbook.store_claim(json.dumps(request.json))
 
-    obag = OrgBookAgent(
-        pool,
-        'The-Org-Book-Agent-0000000000000',
-        'the-org-book-agent-wallet',
-        None,
-        '127.0.0.1',
-        9702,
-        'api/v0')
-    await obag.open()
-    await obag.create_master_secret('secret')
-
-    await obag.store_claim(json.dumps(request.json))
-    await pool.close()
-
-    return json({'success': True})
+    return sanic_json({'success': True})
 
 
 if __name__ == '__main__':
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(boot())
+    loop.close()
     app.run(host="0.0.0.0", port=8000, debug=True)
